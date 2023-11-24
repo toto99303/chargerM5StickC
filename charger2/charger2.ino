1e98d6dcc0fe8a70daa8ea17336657f91843d535
toto99303 <dreamingdies@gmail.com>
2023-11-24 02:12:42 +0200

    Code adjusted for M5StickC
    Code adjusted for M5StickC

--- charger2/charger2.ino
+++ charger2/charger2.ino
@@ -1,4 +1,4 @@
-#include "M5StickCPlus.h"
+#include "M5StickC.h"
 #include "fonts.h"
 TFT_eSprite spr = TFT_eSprite(&M5.Lcd);
 
@@ -34,7 +34,7 @@ void setup(){
   pinMode(37,INPUT_PULLUP);
   pinMode(39,INPUT_PULLUP);
   M5.begin();
-  spr.createSprite(135,240);
+  spr.createSprite(80,160);
   TimeStruct.Hours   = 00;
   TimeStruct.Minutes = 00;
   TimeStruct.Seconds = 00;
@@ -49,53 +49,57 @@ void draw()
   spr.fillSprite(TFT_BLACK);
  
   for(int i=0;i<5;i++)
-  spr.fillRect(100+(i*6),10,4,12,color);
+  spr.fillRect(50+(i*6),1,4,12,color);
 
   for(int i=0;i<chosenB+1;i++)
-  spr.fillRect(100+(i*6),10,4,12,color2);
+  spr.fillRect(50+(i*6),1,4,12,color2);
 
   spr.setTextDatum(0);
-  spr.setTextColor(WHITE,0x0083);
-  spr.drawString("BATTERY",10,8,2);
-  spr.drawString("SET CURRENT     ",10,124,2);
-  spr.setTextColor(WHITE,BLACK);
-  spr.setFreeFont(&DSEG7_Classic_Bold_32);
-  spr.drawString(String(vbat),10,30);
-  spr.drawString("V",100,30,2);
-
-
+  spr.setTextFont(1);
+  spr.setTextColor(WHITE,BLACK); //0083
+  spr.drawString("BAT",2,1,2);
+  spr.drawString("SET CURRENT",2,85);
+  //spr.setTextColor(WHITE,BLACK);
   spr.setFreeFont(&DSEG7_Classic_Bold_17);
-  spr.drawString(hr+":"+mi,10,216);
+  spr.drawString(String(vbat),1,20);
+  spr.drawString("V",50,15,2);
+
+
   spr.setFreeFont(&DSEG7_Classic_Bold_12);
-  spr.drawString(se,76,216);
+  spr.drawString(hr+":"+mi,1,144);
+  spr.setFreeFont(&DSEG7_Classic_Bold_12);
+  spr.drawString(se,52,141);
   
+  spr.setTextFont(1);
   for(int i=0;i<4;i++)
-  spr.drawString(cc[i],70,142+(i*15),2);
+  spr.drawString(cc[i],36,96+(i*10));
   
-  spr.fillCircle(62,150+(chosen*15),4,TFT_RED);
+  spr.fillCircle(31,99+(chosen*10),2,TFT_RED);
   
-  spr.drawFastHLine(10, 144, 40, color2);
-  spr.drawFastVLine(50, 144, 64, color2);
-  spr.drawFastVLine(51, 144, 64, color2);
-  spr.drawFastHLine(50, 208, 74, color2);
+  spr.drawFastHLine(1, 95, 25, color2);
+  spr.drawFastVLine(25, 95, 42, color2);
+  spr.drawFastVLine(26, 95, 42, color2);
+  spr.drawFastHLine(25, 137, 54, color2);
 
-   spr.setTextFont(0);
-   spr.setTextColor(0x4C77,BLACK);
-   spr.drawString("TIME:",12,200);
+  spr.setTextFont(0);
+  spr.setTextColor(0x4C77,BLACK);
+  spr.drawString("TIME",0,131);
    
-   spr.drawString("TEMP:",12,150);
-   spr.drawString(String(temp),12,160);
+  spr.drawString("TEMP",0,98);
+  spr.drawString(String(temp,1),0,108);
   
-  spr.setTextDatum(4);
-  spr.fillRoundRect(10, 72, 55,46,4,color);
-  spr.fillRoundRect(70, 72, 55,46,4,color);
+ // spr.setTextDatum(4);
+  spr.fillRoundRect(0, 42, 38,39,4,color);
+  spr.fillRoundRect(41, 42, 38,39,4,color);
   
+  spr.setTextFont(0);
   spr.setTextColor(WHITE,color);
-  spr.drawString("USB V",37,82,2);
-  spr.drawString("CHR I",97,82,2);
-  spr.setFreeFont(&DSEG7_Classic_Bold_17);
-  spr.drawString(String(vin),37,102);
-  spr.drawString(String(charge),97,102);
+  spr.drawString("USB V",4,45);
+
+  spr.drawString("CHR I",45,45);
+  spr.setFreeFont(&DSEG7_Classic_Bold_12);
+  spr.drawString(String(vin),3,60);
+  spr.drawString(String(charge),50,60);
 
   spr.pushSprite(0,0); 
 }
